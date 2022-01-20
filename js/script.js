23337229eac0ba67c1c58b42fd22ebe377c4b006
aldimhernandez <aldana-hernandez@hotmail.com>
2022-01-20 00:01:34 -0300

    adding carrousel

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 window.onload = () => {
-    /* dinamic menu js */
-    const menuIcon = document.getElementById("menu-icon");
+  /* dinamic menu js */
+  const menuIcon = document.getElementById("menu-icon");
 
-    menuIcon.addEventListener("click", (e) => {
-        document.getElementById("navbar").classList.toggle("show")
-    });
-}
+  menuIcon.addEventListener("click", (e) => {
+    document.getElementById("navbar").classList.toggle("show");
+  })
+
+  /* dinamic carrousel */
+
+  const   imgContainer = document.querySelector('.img-container'),
+          opc = document.querySelectorAll('.opc')
+
+  opc.forEach((eachOpc, i) => {
+    opc[i].addEventListener('click', () => {
+      let position = i
+
+      let operation = position * -10
+
+      imgContainer.style.transform = `translateX(${operation}%)`
+
+      opc.forEach((eachOpc, i) => {
+        opc[i].classList.remove('active')
+      })
+      opc[i].classList.add('active')
+    })
+  })
+};
